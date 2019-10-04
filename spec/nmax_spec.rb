c6d73f9252d02c7cb244ec1a64b1f9412cdddd16
Legatus88 <y1wkn8@gmail.com>
2019-10-04 06:51:30 +0400

    small fix

--- spec/nmax_spec.rb
+++ spec/nmax_spec.rb
@@ -1,9 +1,36 @@
+require_relative '../lib/nmax.rb'
+
 RSpec.describe Nmax do
-  it "has a version number" do
-    expect(Nmax::VERSION).not_to be nil
+
+  describe "when input incorrect" do
+    it "raises IOError" do
+      expect{ Nmax.return_biggest_numbers(2) }.to raise_error(IOError)
+    end
+
+    it "raises IOError" do
+      expect{ Nmax.return_biggest_numbers('awfawf') }.to raise_error(IOError)
+    end
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  describe "when input correct" do
+    before(:all) do
+      STDIN = StringIO.new("123d.,tt /n4234sgrs..,;lj\n 83940sefsf,f,.")
+    end
+
+    it "has a version number" do
+      expect(Nmax::VERSION).not_to be nil
+    end
+
+    it "raises ArgumentError" do
+      expect{ Nmax.return_biggest_numbers(0) }.to raise_error(ArgumentError)
+    end
+
+    it "raises ArgumentError" do
+      expect{ Nmax.return_biggest_numbers('qwerty') }.to raise_error(ArgumentError)
+    end
+
+    it "returns two numbers" do
+      expect(Nmax.return_biggest_numbers(2)).to eq([83940, 4234])
+    end
   end
 end
